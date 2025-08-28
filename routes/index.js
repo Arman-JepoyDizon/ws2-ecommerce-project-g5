af6589897e0d6f3582417f2dfa452771f069ff97
fajardo_alf <fajardo_alf@f213.ub.lan>
2025-08-28 17:54:30 +0800

    Initial Push

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//Home Route
+
+router.get('/', (req, res) => {
+    res.render('index', {title: "Home Page", message: "Hello, MongoDB is conntected!"});
+});
+
+module.exports = router;
